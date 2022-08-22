c29e2520fb268b91ca426da3726421fe3fb7d293
lets00 <eduardovansilva@gmail.com>
2022-08-22 19:57:32 -0300

    Melhorando visualizacao Pais

--- src/components/paises/Pais/Pais.jsx
+++ src/components/paises/Pais/Pais.jsx
@@ -3,7 +3,7 @@ import './Pais.css'
 
 export default function Pais(props) {
   return (
-      <div className='col-12 col-lg-3 borda pais'>
+      <div className='col-7 col-lg-3 borda pais'>
         <img className='bandeira' src={props.bandeira}></img>
         <span>{props.nome ? props.nome : 'País'}</span>
       </div>
