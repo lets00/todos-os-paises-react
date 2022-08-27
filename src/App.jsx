656dab804f56eb9370ef9f115d3972691c5c1869
lets00 <eduardovansilva@gmail.com>
2022-08-27 16:32:12 -0300

    Adicionando API e integrando com o componente Filtro e Paises

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import Filtro from './components/filtro/Filtro'
 import Titulo from './components/titulo/Titulo'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Paises from './components/paises/Paises'
+import { useEffect } from 'react'
 
 function App() {
   const [filtroValue, setFiltroValue] = useState('')
+  const [paises, setPaises] = useState([])
+
+  useEffect(() => {    
+    const coletaDados = async () => {
+      const data = await fetch('https://restcountries.com/v3.1/all')
+      const dataJson = await data.json()
+      const paisesNomeEBandeira = dataJson.map(pais => { return {'nome': pais.translations.por.common.toLowerCase(), 'bandeira': pais.flags.png }} )
+      setPaises(paisesNomeEBandeira)
+    }
+
+    coletaDados().catch((err) => console.log(`erro: ${err}`))
+  }, [])
 
   function handleFiltroValue(value) {
     setFiltroValue(value)
@@ -16,8 +29,7 @@ function App() {
       <div className='container'>
         <Titulo />
         <Filtro onChangeFiltroValue={handleFiltroValue}/>
-        <Paises />
-        <p>{filtroValue}</p>
+        <Paises listaDePaises={paises.filter( pais => pais.nome.includes(filtroValue.toLowerCase())) }/>
       </div>
     </main>
   )
